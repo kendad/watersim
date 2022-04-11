e97a6ec32a2483035cf0cd951aac3196eda94263
KAUSHIK ISWARY <kaushikiswary@gmail.com>
2022-04-11 08:51:16 +0530

    Add project files.

---
+++ watersim/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include "globals.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+#include "shader.h"
+#include "texture.h"
+#include "camera.h"
+#include "model.h"
+
+//screen settings
+const int SCR_WIDTH = 800;
+const int SCR_HEIGHT = 600;
+
+//timing
+float deltaTime = 0.0f;
+float lastTime = 0.0f;
+
+void main() {
+	//intialize GLFW & GLAD
+	INIT();
+
+	//Camera Class
+	Camera camera(window, glm::vec3(0.0f, 0.0f, 3.0f), -90.0f, 0.0f, SCR_WIDTH / 2, SCR_HEIGHT / 2);
+	gCamera = &camera;//update the global camera variable
+
+	//#######################################################################################################################
+	//load the model here
+	Shader ourShader("Shaders/water.vs", "Shaders/water.fs");
+	Model ourModel("resources/plane/plane.obj");
+	
+	//set projection for 3D MODEL
+	glm::mat4 projection = glm::mat4(1.0f);
+	projection = glm::perspective(glm::radians(45.0f), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+	ourShader.use();
+	glUniformMatrix4fv(glGetUniformLocation(ourShader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+	//render Loop
+	glEnable(GL_DEPTH_TEST);//enable depth testing
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);//enable drawing in wireframe mode
+	//glEnable(GL_CULL_FACE);
+	while (!glfwWindowShouldClose(window)) {
+		//get time
+		float currentTime = (float)glfwGetTime();
+		deltaTime = currentTime - lastTime;
+		lastTime = currentTime;
+
+		//process user input
+		processInput(window,deltaTime);
+
+		//all render stuff goes here
+		glClearColor(0, 0.0f, 0, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+
+		//Update view Matrix
+		glm::mat4 view = glm::lookAt(gCamera->position, gCamera->position + gCamera->front, gCamera->up);
+		//set view for 3D MODEL
+		ourShader.use();
+		glUniformMatrix4fv(glGetUniformLocation(ourShader.ID, "view"), 1, GL_FALSE, glm::value_ptr(view));
+
+		//update and render 3D MODEL
+		ourShader.use();
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+		glUniformMatrix4fv(glGetUniformLocation(ourShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(model));
+		ourModel.Draw(ourShader);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	//exit
+	glfwTerminate();
+	return;
+}
