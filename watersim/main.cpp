66716c510e75a6ab8b13e8fe5c3a56f82ed72551
KAUSHIK ISWARY <kaushikiswary@gmail.com>
2022-04-11 11:28:46 +0530

    Add water Texture

--- watersim/main.cpp
+++ watersim/main.cpp
@@ -31,6 +31,11 @@ void main() {
 	//load the model here
 	Shader ourShader("Shaders/water.vs", "Shaders/water.fs");
 	Model ourModel("resources/plane/plane.obj");
+
+	//load texture
+	ourShader.use();
+	Texture texture("Assets/water.jpg",false,false);
+	texture.activate(ourShader.ID, "texture1", 1);
 	
 	//set projection for 3D MODEL
 	glm::mat4 projection = glm::mat4(1.0f);
@@ -40,7 +45,7 @@ void main() {
 
 	//render Loop
 	glEnable(GL_DEPTH_TEST);//enable depth testing
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);//enable drawing in wireframe mode
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);//enable drawing in wireframe mode
 	//glEnable(GL_CULL_FACE);
 	while (!glfwWindowShouldClose(window)) {
 		//get time
