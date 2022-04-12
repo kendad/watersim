af08eb9fbc6a083de3d2f91616aeaa5b6aa1cf2b
KAUSHIK ISWARY <kaushikiswary@gmail.com>
2022-04-12 08:23:04 +0530

    Updated phong Lighting system

--- watersim/Shaders/water.fs
+++ watersim/Shaders/water.fs
@@ -15,23 +15,23 @@ uniform vec3 cameraPosition;
 void main()
 {   //Ambient Light
 	float ambientStrength=0.1;
-	vec3 ambient=ambientStrength*lightColor;
+	vec3 ambient=ambientStrength*lightColor*vec3(texture(texture_diffuse1, TexCoords));
 	
 	//Diffuse Light
 	vec3 norm=normalize(Normal);
 	vec3 lightDir=normalize(lightPosition-FragPos);
 	float diff=max(dot(norm,lightDir),0.0);
-	vec3 diffuse=diff*lightColor;
+	vec3 diffuse=diff*lightColor*vec3(texture(texture_diffuse1, TexCoords));
 	
 	//Specular Light
 	float specularStrength=0.5f;
 	vec3 viewDir=normalize(cameraPosition-FragPos);
 	vec3 reflectDir=(-lightDir,norm);
-	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-	vec3 specular = specularStrength * spec * lightColor;  
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 256);
+	vec3 specular = specularStrength * spec * lightColor*vec3(texture(texture_diffuse1, TexCoords));  
 	
 	vec3 result=(ambient+diffuse+specular)*waterColor;
 	
-    FragColor = texture(texture_diffuse1, TexCoords)*vec4(result,1.0f);
-	//FragColor=vec4(result,1.0f);
+    //FragColor = texture(texture_diffuse1, TexCoords)*vec4(result,1.0f);
+	FragColor=vec4(result,1.0f);
 }
